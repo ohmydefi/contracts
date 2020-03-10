cffa4ab82172be04bd10d1d4db562135c556ff01
Robsonsjre <robsonsjre@gmail.com>
2020-03-10 08:17:48 -0300

    changed some parts of script

--- scripts/init-mainnet-from-scratch.sh
+++ scripts/init-mainnet-from-scratch.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e -o pipefail
 
-if [[ -z $MY_ADDRESS ]]; then
-    echo "You should export an environment variable containing your ETH address:\n"
-    echo "export MY_ADDRESS=\"0x...\""
-    exit 1
-fi
-
-OZ_FILE="../.openzeppelin/mainnet.json"
+OZ_FILE="./.openzeppelin/mainnet.json"
 rm -f $OZ_FILE
 
 npx oz session --network mainnet --no-interactive
@@ -15,15 +9,19 @@ npx oz push
 
 # Token mainnet addresses
 DAI_ADDRESS="0x6b175474e89094c44da98b954eedeac495271d0f"
+STRIKE_ADDRESS="0x6b175474e89094c44da98b954eedeac495271d0f"
 USDC_ADDRESS="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+STRIKE_PRICE="230000000"
+STRIKE_PRICE_DECIMALS="6"
+# # Creates the option series
 
 # Estimate block number for Dec 3, 2019 == Nov 19 + 80640 blocks
 # 80640 == (14 days * 24 hours * 60 min * 4 blocks/min)
-EXPIRATION_BLOCK="9050000"
+EXPIRATION_BLOCK="9698679"
 
 # Creates the option series
-npx oz create Option --init initialize --args "\"ohDAI 1:1 USDC A\",\"OHDAI:USDC:A\",$DAI_ADDRESS,18,$USDC_ADDRESS,1000000,$EXPIRATION_BLOCK" --skip-compile
-OPTION_ADDRESS=`cat $OZ_FILE | jq '.proxies["contracts/Option"][0].address' -r`
+npx oz create PutETH --init initialize --args "\"pohETH 230:1 DAI\",\"POHETH:DAI\",$STRIKE_ADDRESS,$STRIKE_PRICE,$STRIKE_PRICE_DECIMALS, $EXPIRATION_BLOCK"
+OPTION_ADDRESS=`cat $OZ_FILE | jq '.proxies["contracts/PutETH"][0].address' -r`
 
 echo "\n\nSummary:\n"
 echo "My address: $MY_ADDRESS"
